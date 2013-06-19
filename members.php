97a112c9d50c884493df840f1c31dc09881b2e0e
Krishnan. N <scratch@srijan.in>
2013-06-19 13:53:29 +0530

    Added quotes to each column and changes the column names to conform to plugin.
    Using plugin: http://wordpress.org/plugins/import-users-from-csv/

--- members.php
+++ members.php
@@ -1,26 +1,31 @@
 <?php
 /* member.php -- Get all users from members table */
 
+/*
+ * Format required by user csv plugin: http://wordpress.org/plugins/import-users-from-csv/
+"user_login","user_email","user_pass","first_name","last_name","display_name","role","custom_usermeta_1","custom_usermeta_2"
+"johndoe","john.doe@localhost.localdomain",,"John","Doe","John Doe","administrator","Test","Test"
+"janedoe","jane.doe@localhost.localdomain.com","test","Jane","Doe","Jane Doe","contributor","0",
+*/
+
 include_once("tt_lib.php");
 
 $fd = tt_connect();
-
-/*printf ("Print out all the fields of members array:  \n"); 
+/*
+printf ("Print out all the fields of members array:  \n"); 
 tt_print_fields("members");
 */
 
 $qry_result = tt_select_fields($fd, "members", array("members_name", "members_email", "members_username", "members_status","members_last_activity_date", "members_registration_date"));
 
-printf ("Member name, email, registration date, last activity date, status\n");
+print ('"user_login","user_email","user_pass","first_name","last_name","display_name","role","custom_usermeta_1","custom_usermeta_2'."\n");
 while (($members = mysql_fetch_array($qry_result, MYSQL_ASSOC))) {
-   printf ("%s, %s, %s, %s, %s\n", $members['members_name'], $members['members_email'], $members['members_registration_date'], 
-           $members['members_last_activity_date'], $members['members_status']);
+   printf ("\"%s\",\"%s\",,\"%s\",,\"%s\",,,\n", $members['members_username'], $members['members_email'], $members['members_name'], $members['members_name']);
 }
-print("Done\n");
 
 $retVal = mysql_close($fd);
 if (!$retVal) {
-	die("Error closing mysql connection");
+	printf("Error closing mysql connection: %s\n", mysql_error());
 }
 ?>
 

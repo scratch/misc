bab11feee140934c1afe5016b490dd191f2998ea
Krishnan. N <scratch@srijan.in>
2013-07-16 15:01:31 +0530

    Added email field.

--- blogs.php
+++ blogs.php
@@ -3,7 +3,7 @@ include_once ("tt_lib.php");
 
 /* Query for comments for a specific blog */
 function blog_comments($blog_id, $fd) {
-	$blog_cmt_qry = "SELECT m.members_name, bec.blog_entries_comments_blog_entries_id, bec.blog_entries_comments_date, bec.blog_entries_comments_comment " . " FROM members m, blog_entries_comments bec " . " WHERE bec.blog_entries_comments_blog_entries_id=$blog_id " . " AND m.members_id = bec.blog_entries_comments_author_id";
+	$blog_cmt_qry = "SELECT m.members_name, m.members_email, bec.blog_entries_comments_blog_entries_id, bec.blog_entries_comments_date, bec.blog_entries_comments_comment " . " FROM members m, blog_entries_comments bec " . " WHERE bec.blog_entries_comments_blog_entries_id=$blog_id " . " AND m.members_id = bec.blog_entries_comments_author_id";
 
 	$qry_result = mysql_query($blog_cmt_qry, $fd);
 	if (!$qry_result) {
@@ -18,7 +18,7 @@ $fd = tt_connect();
 $qry_result = tt_select_fields($fd, "blogs", array("blogs_title", "blogs_description"));
 
 /* Get data about blogs from blogs and blog_entries table. From members, get the bloggers name (based on ID) */
-$blog_entry_qry = "SELECT m.members_name, b.blogs_title, b.blogs_description, be.blog_entries_id, be.blog_entries_blogs_id, be.blog_entries_title, be.blog_entries_content, be.blog_entries_last_updated_on " . " FROM blogs b" . " INNER JOIN members m ON m.members_id=b.blogs_author_id " . " INNER JOIN blog_entries be  ON m.members_id=be.blog_entries_id ";
+$blog_entry_qry = "SELECT m.members_name, m.members_email, b.blogs_title, b.blogs_description, be.blog_entries_id, be.blog_entries_blogs_id, be.blog_entries_title, be.blog_entries_content, be.blog_entries_last_updated_on " . " FROM blogs b" . " INNER JOIN members m ON m.members_id=b.blogs_author_id " . " INNER JOIN blog_entries be  ON m.members_id=be.blog_entries_id ";
 $qry_result = mysql_query($blog_entry_qry, $fd);
 if (!$qry_result) {
 	printf("QUERY FAILED: %s", mysql_error());
@@ -31,8 +31,8 @@ if (!$fd_blogcsv || !$fd_blogcmtscsv) {
 	die();
 }
 
-fputcsv($fd_blogcsv, split(',', "blog_id,author,title,date,content"));
-fputcsv($fd_blogcmtscsv, split(',', "blog_id,comment_author,comment_date,comment_content"));
+fputcsv($fd_blogcsv, split(',', "blog_id,author,email,title,date,content"));
+fputcsv($fd_blogcmtscsv, split(',', "blog_id,comment_author,comment_email, comment_date,comment_content"));
 
 $blog_entry = array();
 
@@ -42,8 +42,13 @@ while (($blog_entry = mysql_fetch_array($qry_result, MYSQL_ASSOC))) {
 		die("Error fetching array from mysql object");
 	}
 
-	fputcsv($fd_blogcsv, array($blog_entry['blog_entries_id'], $blog_entry['members_name'], $blog_entry['blog_entries_title'], 
-		$blog_entry['blog_entries_last_updated_on'], $blog_entry['blog_entries_content']));
+	fputcsv($fd_blogcsv,
+ array($blog_entry['blog_entries_id'],
+ $blog_entry['members_name'],
+ $blog_entry['members_email'],
+ $blog_entry['blog_entries_title'],
+ $blog_entry['blog_entries_last_updated_on'],
+ $blog_entry['blog_entries_content']));
 	printf("Author Name: %s\nTitle:  %s\nDate: %s\nblog id: %s\nblog entries id: %s\n\nContent:\n%s\n\nXXX---------\n", 
 	$blog_entry['members_name'], $blog_entry['blog_entries_title'], $blog_entry['blog_entries_last_updated_on'], 
 	$blog_entry['blog_entries_id'], $blog_entry['blog_entries_blogs_id'], $blog_entry['blog_entries_content']);
@@ -54,7 +59,12 @@ while (($blog_entry = mysql_fetch_array($qry_result, MYSQL_ASSOC))) {
 
 	while (($cmt_entry = mysql_fetch_array($cmt_result, MYSQL_ASSOC)))  {
 		printf("\nComment Author: %s\nComment Date: %s\nComment: %s\n XX--comment-end--XX \n\n", $cmt_entry['members_name'], $cmt_entry['blog_entries_comments_date'], $cmt_entry['blog_entries_comments_comment']);
-		fputcsv($fd_blogcmtscsv, array($cmt_entry['blog_entries_comments_blog_entries_id'],$cmt_entry['members_name'], $cmt_entry['blog_entries_comments_date'], $cmt_entry['blog_entries_comments_comment']));
+		fputcsv($fd_blogcmtscsv,
+ array($cmt_entry['blog_entries_comments_blog_entries_id'],
+$cmt_entry['members_name'],
+$cmt_entry['members_email'],
+ $cmt_entry['blog_entries_comments_date'],
+ $cmt_entry['blog_entries_comments_comment']));
 	}
 }
 ?>
